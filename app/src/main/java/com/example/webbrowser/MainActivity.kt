332313022ad46a163dec4752b9eb6c44d6904219
k906506 <k906506@naver.com>
2021-09-22 10:05:02 +0900

    Default 페이지, 페이지 연결 구현

--- app/src/main/java/com/example/webbrowser/MainActivity.kt
+++ app/src/main/java/com/example/webbrowser/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.webbrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+        bindViews()
+    }
+
+    private fun initView() {
+        webView.apply {
+            webViewClient = WebViewClient() // 외부 웹브라우져로 전환되는 것을 막음
+            settings.javaScriptEnabled = true // 자바스크립트 사용을 허용
+            loadUrl("https://www.google.com")
+        }
+    }
+
+    private fun bindViews() {
+        addressBar.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl(v.text.toString())
+            }
+            return@setOnEditorActionListener false
+        }
     }
 }
